87daf9c4a7e4bf370197db512f7ca57548a56f33
Juan Gabriel <juancentenob@gmail.com>
2023-02-11 08:17:10 -0600

    comentarios

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 function changeBackground(){
+    //añade o quita al html <body> la clase "dark" (<body class="dark">)
     document.body.classList.toggle("dark");
+    //añade o quita al boton id="light-button" la clase lightcolor (esto es una excepcion para cuando estoy sobre el boton de "dark" me cambie el color de letra del boton de "light")
     document.getElementById("light-button").classList.toggle("lightcolor");
 }
 function themSelected(theme){
+    //oculta el selector de dark o light (d-none es una clase que solo pone display:none)
     document.getElementById("theme_selector").classList.add("d-none");
+    //muestra la pagina principal quitando la clase "d-none"
     document.getElementById("main_page").classList.remove("d-none");
-
+    //pone la cookie para recordar que el tema seleccionado es dark o light
     setCookie("theme", theme, 30);
 }
 
+//setea una cookie
 function setCookie(cname, cvalue, exdays) {
     const d = new Date();
     d.setTime(d.getTime() + (exdays*24*60*60*1000));
     let expires = "expires="+ d.toUTCString();
     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
+//lee una cookie
 function getCookie(cname) {
     let name = cname + "=";
     let decodedCookie = decodeURIComponent(document.cookie);
@@ -33,11 +39,14 @@ function getCookie(cname) {
 
 
 document.addEventListener("DOMContentLoaded", function(event) {
+    //revisa si ya hay un tema seleccionado
     let theme = getCookie('theme');
     if(theme !== undefined && theme !== "")
     {
+        //si el tema seleccionado es dark le pone la clase al body para que se aplique el css correspondiente 
         if(theme === "dark")
             document.body.classList.add("dark");
+        //oculta la pagina de seleccion de tema y muestra la principal
         themSelected(theme);
     }
 });
