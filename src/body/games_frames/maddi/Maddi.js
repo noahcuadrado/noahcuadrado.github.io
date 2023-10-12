2bccea463dc0a5874359779bf75ae48418726d6e
Juan Gabriel <juancentenob@gmail.com>
2023-10-12 06:08:59 -0600

    games centered

--- src/body/games_frames/maddi/Maddi.js
+++ src/body/games_frames/maddi/Maddi.js
@@ -3,7 +3,7 @@ import { React } from 'react';
 function Maddi() 
 {
   return (
-    <div className="body p-3">
+    <div className="body p-3 d-flex justify-content-center">
         <iframe title="Maddi" frameborder="0" src="https://itch.io/embed-upload/6983596?color=510B09" allowfullscreen="" width="1280" height="740">
           <a href="https://noahuxui.itch.io/maddi">Play MADDI on itch.io</a>
         </iframe>
--- src/body/games_frames/type/Type.js
+++ src/body/games_frames/type/Type.js
@@ -3,7 +3,7 @@ import { React } from 'react';
 function Type() 
 {
   return (
-    <div className="body p-3">
+    <div className="body p-3 d-flex justify-content-center">
         <iframe title="Type." frameborder="0" src="https://itch.io/embed-upload/7031635?color=000000" allowfullscreen="" width="720" height="740">
           <a href="https://noahuxui.itch.io/type">Play Type.| on itch.io</a>
         </iframe>
